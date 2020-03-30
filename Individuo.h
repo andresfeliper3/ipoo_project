5032fd853cbb305868cfe9969e83a234bed03321
andresfeliper3 <andresfeliper.2001@gmail.com>
2020-03-30 12:31:25 -0500

    primera parte de la clase individuo, primer commit

---
+++ Individuo.h
@@ -0,0 +1,17 @@
+/*
+  Archivo: Individuo.h
+  Autor: Andrés Rincón
+  Fecha creación: 2020-03-30
+  Fecha última modificación: 2020-03-30
+*/
+
+#ifndef INDIVIDUO_HH
+#define INDIVIDUO_HH
+
+class Individuo 
+{
+
+};
+#else
+class Individuo;
+#endif
