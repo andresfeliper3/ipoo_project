c6be2a22311609c19e00ede2f8d76b0f5c5dfe06
andresfeliper3 <andresfeliper.2001@gmail.com>
2020-03-30 12:48:17 -0500

    constructor y función agregarPresa para clase Individuo

--- Individuo.h
+++ Individuo.h
@@ -8,9 +8,25 @@
 #ifndef INDIVIDUO_HH
 #define INDIVIDUO_HH
 
+#include <vector>
+#include <string>
+using namespace std;
+
 class Individuo 
 {
+    private:
+        string nombre;
+        string letraParaMover; //Letra asociada al movimiento
+        vector <Individuo *> puedeComer; //Bichos que puede comer
 
+    public:
+        //Constructor
+        Individuo(string nombre, string letraParaMover);
+
+        //Agrega posibles presas para el Individuo, añadiendo punteros al vector puedeComer
+        virtual void agregarPresa(Individuo *);  
+
+        //Añadir función "crearBichos" 
 };
 #else
 class Individuo;
