d066037faa1536b2365fde0e543e41caaf239112
andresfeliper3 <andresfeliper.2001@gmail.com>
2020-04-08 17:32:36 -0500

    creacion de Lugar.cpp

---
+++ Lugar.cpp
@@ -0,0 +1,103 @@
+/*
+  Archivo: Lugar.cpp
+  Autores: Andrés Rincón
+  Fecha creación: 2020-abril-08
+  Fecha última modificación: 2020-abril-08
+*/
+
+#include "Lugar.h"
+
+//Constructor
+Lugar::Lugar(Lugar *lugarVecino) : lugarVecino(lugarVecino)
+{
+
+}
+
+//Destructor
+Lugar::~Lugar()
+{
+
+}
+
+//Agrega un individuo al vector de individuos, si ya no está presente
+void Lugar::agregarIndividuo(Individuo *individuo)
+{
+    if(individuoPresente(individuo) == -1) //Si el individuo no está presente
+    {
+        individuosPresentes.push_back(individuo);
+    }
+}
+
+//DUDA, SI USAR ERASE
+//Quita un individuo del vector de individuo, si está presente
+void Lugar::quitarIndividuo(Individuo *individuo)
+{
+    if(individuoPresente(individuo) != -1)
+    {
+        individuosPresentes.erase(individuosPresentes.begin()+individuoPresente(individuo)); 
+    }
+}
+
+//Si tiene un lugar vecino, lo quita
+void Lugar::quitarLugarVecino() //POSIBLEMENTE CAMBIAR ESTA FUNCIÓN DE VOID A BOOL
+{
+    if(lugarVecino)
+    {
+        lugarVecino = nullptr;
+    }
+}
+
+//Si no tiene un lugar vecino, agrega uno
+void Lugar::agregarLugarVecino(Lugar *nuevoLugarVecino)
+{
+    if(!lugarVecino)
+    {
+        lugarVecino = nuevoLugarVecino;
+    }
+}
+
+//Retorna la posición si el individuo ingresado está presente en el vector de individuos de la parte privada, retorna -1 en caso en contrario
+int Lugar::individuoPresente(Individuo *individuo)
+{
+    for(int cualIndividuo=0; cualIndividuo < individuosPresentes.size(); cualIndividuo++)
+    {
+        if(individuosPresentes[cualIndividuo] == individuo) 
+        {
+            
+            return cualIndividuo;
+        }
+    }
+    return -1;
+}
+
+//Hace que un individuo se mueva de este lugar al lugar vecino. Retorna true si logra hacerlo, false en caso contrario
+bool Lugar::moverIndividuo(Individuo *individuo)
+{
+    //AGREGAR DESPUÉS QUE SÓLO SI EL ROBOT ESTÁ PRESENTE, SE PUEDE MOVER UN INDIVIDUO
+    if(individuoPresente(individuo) != -1) //Si el individuo está presente
+    {
+        quitarIndividuo(individuo);
+        lugarVecino->agregarIndividuo(individuo);
+        return true;
+    }
+    return false; 
+}
+
+//Revisa si el jugador ha ganado o perdido la partida
+void Lugar::revisarPartida()
+{
+ /*Realizar un ciclo que busque por todos los individuos del vector.
+ Cada individuo debe ver si se puede comer a los demás.
+ */
+}
+
+//Anuncia la victoria
+string Lugar::victoria()
+{
+    return "¡Has ganado!";
+}
+//Anuncia la derrota
+string Lugar::derrota()
+{
+    return "¡Has perdido!";
+}
